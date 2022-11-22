71f5538d16392d28e68c33b2520f11b123e1a3b8
北野 <1017895392@qq.com>
2022-11-22 19:22:40 +0800

    chore: subcode

--- src/components/widgets/Header/index.vue
+++ src/components/widgets/Header/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <header class="flex items-center h-14 px-4">
-    header
+  <header class="flex items-center h-14 px-4 shadow">
+    <Logo/>
   </header>
 </template>
 
 <script setup lang="ts">
-
+import Logo from './Logo.vue'
 </script>
 
 <style scoped lang="scss">
