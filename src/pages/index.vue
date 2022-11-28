54094d5425577e1b016d04fe3a201b5c8286525f
北野 <1017895392@qq.com>
2022-11-28 19:52:04 +0800

    fix: line-clamp

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,7 +13,7 @@
         class="flex items-center box-border flex-col flex-col-reverse md:odd:flex-row md:even:flex-row-reverse h-80 mb-32"
         v-for="item in 10" :key="item">
         <div class="h-1/3 md:h-64 w-full md:w-1/2 rounded-lg border-dashed border-2 p-4 box-border ">
-          <div class="w-full h-28 ellipsis">
+          <div class="w-full indent-8 line-clamp-3 md:line-clamp-4">
             庆历四年春，滕子京谪守巴陵郡。越明年，政通人和，百废具兴，乃重修岳阳楼，增其旧制，刻唐贤今人诗赋于其上，属予作文以记之。
             予观夫巴陵胜状，在洞庭一湖。衔远山，吞长江，浩浩汤汤，横无际涯，朝晖夕阴，气象万千，此则岳阳楼之大观也，前人之述备矣。然则北通巫峡，南极潇湘，迁客骚人，多会于此，览物之情，得无异乎？
             若夫淫雨霏霏，连月不开，阴风怒号，浊浪排空，日星隐曜，山岳潜形，商旅不行，樯倾楫摧，薄暮冥冥，虎啸猿啼。登斯楼也，则有去国怀乡，忧谗畏讥，满目萧然，感极而悲者矣。
@@ -33,11 +33,5 @@
 </script>
 
 <style scoped lang="scss">
-.ellipsis {
-  overflow:hidden;
-  text-overflow:ellipsis;
-  display:-webkit-box;
-  -webkit-box-orient:vertical;
-  -webkit-line-clamp:5;
-}
+
 </style>
