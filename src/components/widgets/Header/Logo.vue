190fbdad9cfae6f891913adcae2e34605ba1a9b0
北野 <1017895392@qq.com>
2022-12-23 17:30:34 +0800

    feat: canvas

--- src/components/widgets/Header/Logo.vue
+++ src/components/widgets/Header/Logo.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="text-2xl font-extrabold leading-snug bg-clip-text text-transparent bg-gradient-to-br from-green-400 via-emerald-400 to-cyan-400 animate-roll cursor-pointer select-none" @click="back">
-    Cyclone
-  </div>
+  <!--  <div class="text-2xl font-extrabold leading-snug bg-clip-text text-transparent bg-gradient-to-br from-green-400 via-emerald-400 to-cyan-400 animate-roll cursor-pointer select-none" @click="back">-->
+  <!--    Cyclone-->
+  <!--  </div>-->
+  <nuxt-link to="/">
+    <canvas ref="refCanvas" width="100" height="56"></canvas>
+  </nuxt-link>
 </template>
 
 <script setup lang="ts">
-import {useRouter} from "#imports";
+import { onMounted, ref } from "#imports";
 
-const router = useRouter()
+const refCanvas = ref<HTMLCanvasElement | null>()
 
-const back = () => {
-  router.push({path: '/'})
+onMounted(() => {
+  useCanvas()
+})
+
+const useCanvas = () => {
+  const canvas = refCanvas.value
+  if (canvas) {
+    const ctx = canvas.getContext('2d')
+    if (ctx) {
+      ctx.fillText('Cyclone', 35, 20)
+    }
+  }
 }
 </script>
 
